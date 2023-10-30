3edebf5e44bd8d91400246326507db78ec1cf0e8
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-10-30 09:09:48 +0000

    애니메이션 추가

--- src/components/GraphicSample/Computer.tsx
+++ src/components/GraphicSample/Computer.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import "./computer.scss";
+
+const tImg = "/images/computer/tums.png";
+
 const ComputerSample = () => {
-  return <div>컴퓸터 화면</div>;
+  return (
+    <div id="computer-graphic-wrap">
+      <p>컴퓸터 화면</p>
+
+      <Image src={tImg} alt="co" width={300} height={300} className="conputer-img" />
+    </div>
+  );
 };
 
 export default ComputerSample;
