6a35f128ae28674cad924f3f21798535fbee1ea4
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-11-03 02:20:45 +0900

    api 요청 최적화

--- src/app/user/[id]/profile/page.tsx
+++ src/app/user/[id]/profile/page.tsx
@@ -1,25 +1,27 @@
 "use client";
 
 import { useState } from "react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function Profile({ params }: { params: { id: string } }) {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const [boardList, setBoardList] = useState([]);
-  const fetchData = async () => {
-    const list = await fetch(`http://localhost:3000/api/user/${params.id}`, {
-      method: "GET",
-      headers: {
-        authorization: session?.user.accessToken || "",
-      },
-    }).then((res) => res.json());
-    setBoardList(list.data);
-  };
+  // const fetchData = async () => {
+  //   const list = await fetch(`${BASE_URL}/api/user/${params.id}`, {
+  //     method: "GET",
+  //     headers: {
+  //       authorization: session?.user.accessToken || "",
+  //     },
+  //   }).then((res) => res.json());
+  //   setBoardList(list.data);
+  // };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       내프로필
-      <button onClick={() => fetchData()}>click</button>
+      {/* <button onClick={() => fetchData()}>click</button> */}
       {JSON.stringify(boardList)}
     </main>
   );
--- src/services/login.ts
+++ src/services/login.ts
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.NEXTAUTH_URL;
 
 export const postData = async (credentials: Record<"email" | "password", string> | undefined, req: any) => {
-  return await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
+  return await fetch(`${BASE_URL}/api/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
