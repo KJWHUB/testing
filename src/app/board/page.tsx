aa7c9f7d52ab599e70586abadca2bdd06980397f
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-11-08 00:21:45 +0900

    api 수정

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,32 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import Link from "next/link";
 import BasicButton from "@/components/Button/BasicButton";
 import BasicCard from "@/components/Card/BasicCard";
 import { getList } from "@/services/board";
-export default async function Board() {
-  const res = await getList();
-  const { list } = await res.json();
+
+export default function Board() {
+  const [boardList, setBoardList] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getList();
+      const { list } = await res.json();
+      setBoardList(list);
+    };
+    fetch();
+  }, []);
 
   return (
     <main className="p-5">
-      {res.ok &&
-        list.map((el: Board, i: number) => {
-          return (
-            <div className="pb-3" key={i}>
-              <BasicCard>
-                <div className="mt-2">
-                  <h4 className="mb-2 text-lg">{el.title}</h4>
-                  <p className="text-sm">{el.content}</p>
-                  <div className="flex flex-row-reverse">
-                    <p className=" text-xs text-gray-400">
-                      <span className="mr-1">작성일</span> {dayjs(el.createAt).format("YYYY-MM-DD HH:mm")}
-                    </p>
-                  </div>
+      {boardList.map((el: Board, i: number) => {
+        return (
+          <div className="pb-3" key={i}>
+            <BasicCard>
+              <div className="mt-2">
+                <h4 className="mb-2 text-lg">{el.title}</h4>
+                <p className="text-sm">{el.content}</p>
+                <div className="flex flex-row-reverse">
+                  <p className=" text-xs text-gray-400">
+                    <span className="mr-1">작성일</span> {dayjs(el.createAt).format("YYYY-MM-DD HH:mm")}
+                  </p>
                 </div>
-              </BasicCard>
-            </div>
-          );
-        })}
+              </div>
+            </BasicCard>
+          </div>
+        );
+      })}
 
       <div className="flex flex-row-reverse mt-8">
         <Link href={"board/write"}>
